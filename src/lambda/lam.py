6d63d9578f847c3e21a04f31d25a80b5ed88061e
Abdullah Mohammed <abdullah.4mohamed@gmail.com>
2024-04-04 16:51:30 +0100

    lamda code

---
+++ src/lambda/lam.py
@@ -0,0 +1,18 @@
+import json
+
+def lambda_handler(event, context):
+    # Sample response data
+    response_data = {
+        "message": "Hello from Lambda!"
+    }
+    
+    # Construct HTTP response
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(response_data)
+    }
+    
+    return response
