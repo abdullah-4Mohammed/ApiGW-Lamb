f145fc1e90806ffec0e0a32c03e96d0824a6075b
Abdullah Mohammed <abdullah.4mohamed@gmail.com>
2024-04-07 17:36:27 +0100

    changing the pathpart

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "test" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id = aws_api_gateway_rest_api.api.root_resource_id
-  path_part = "lam"
+  path_part = "test1"
 }
 
 resource "aws_api_gateway_method" "api_method" {
