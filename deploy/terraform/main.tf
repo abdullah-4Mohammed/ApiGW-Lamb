7b6f09d8fc3974bc93a84e62b1d20ef75d5f206e
Abdullah Mohammed <abdullah.4mohamed@gmail.com>
2024-04-07 18:17:50 +0100

    changing the api permission to block than role

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -43,10 +43,10 @@ resource "aws_api_gateway_integration" "api_integration" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.test.id
   http_method = aws_api_gateway_method.api_method.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type = "AWS_PROXY"
   uri = aws_lambda_function.lam.invoke_arn
-  credentials = aws_iam_role.role.arn
+  #credentials = aws_iam_role.role.arn
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
